22dd878a884799b4f0b33f2559c9c30627d784c9
p-a <peter.augustinak@gmail.com>
2021-12-02 20:26:21 +0100

    Initial commit, all exercises chapter 02

---
+++ src/chapter02.py
@@ -0,0 +1,49 @@
+#2-1
+message = "Hello chapter 02!"
+print(message)
+
+#2-2
+message = "Changed message!"
+print(message)
+
+#2-3
+name = "Peter"
+print(f"Hello {name}, how are you?")
+
+#2-4
+name = "thoMAS"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+#2-5
+print('Scott MacGregor\'s important quote is "I think its very important, if you have the luxury of choosing jobs,'
+      ' to choose one that\'s fun."')
+
+#2-6
+famous_person = 'Scott MacGregor'
+print(f'{famous_person}\'s important quote is "I think its very important, if you have the luxury of choosing jobs,'
+      ' to choose one that\'s fun."')
+
+#2-7
+name = '\t  Will Smith \n'
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#2-8
+print(3+5)
+print(12-4)
+print(2*4)
+print(16/2)
+
+#2-9
+my_favourite_number = 34
+print(f"My favourite number is {my_favourite_number}, so you know now!")
+
+#2-10
+# all comments above
+
+#2-11
+import this
