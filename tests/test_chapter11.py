5912478a045a59d665880b7030187f27cd995e0e
p-a <peter.augustinak@gmail.com>
2021-12-09 20:50:36 +0100

    Chapter 11 - Testing your code + giignore updates

---
+++ tests/test_chapter11.py
@@ -0,0 +1,30 @@
+import unittest
+from src.chapter11 import *
+
+
+class CityCountryTest(unittest.TestCase):
+
+    def test_city_country(self):
+        result = city_functions("santiago", "chile")
+        self.assertEqual(result, "Santiago, Chile")
+
+    def test_city_country_population(self):
+        result = city_functions_population("santiago", "chile")
+        self.assertEqual(result, "Santiago, Chile, 500000")
+
+
+class EmployeeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.employee = Employee("John", "Smith", 20000)
+
+    def test_give_default_raise(self):
+        self.assertEqual(self.employee.annual_salary, 20000)
+
+    def test_give_custom_raise(self):
+        self.employee.give_raise(10000)
+        self.assertEqual(self.employee.annual_salary, 30000)
+
+
+if __name__ == '__main__':
+    unittest.main()
